2710cc73a8e3338f98f5ca2573ebca0249132c78
KorobkaVadim <34455@mail.ua>
2019-04-11 14:01:03 +0300

    Implement WebPack 4 support
    https://github.com/ushu/node-addon-loader/issues/6

--- index.js
+++ index.js
@@ -1,60 +1,60 @@
-var loaderUtils = require("loader-utils");
-var path = require("path");
+var loaderUtils = require('loader-utils')
+var path = require('path')
 
 module.exports = function(content) {
-  this.cacheable && this.cacheable();
+  this.cacheable && this.cacheable()
 
   // defaults
   var config = {
-    name: "[hash].[ext]",
+    name: '[hash].[ext]',
     basePath: undefined,
     rewritePath: undefined,
     relativePath: false
-  };
+  }
   // Parse query
-  var query = loaderUtils.getOptions(this) || {};
-  var options = this.options[query.config || "nodeAddonLoader"] || {};
+  var query = loaderUtils.getOptions(this) || {}
+  var options = this._compiler.options[query.config || 'nodeAddonLoader'] || {}
   // options takes precedence over config
   Object.keys(options).forEach(function(attr) {
-    config[attr] = options[attr];
-  });
+    config[attr] = options[attr]
+  })
   // query takes precedence over config and options
   Object.keys(query).forEach(function(attr) {
-    config[attr] = query[attr];
-  });
+    config[attr] = query[attr]
+  })
 
   // Build the output file name
   var url = loaderUtils.interpolateName(this, config.name, {
-    context: this.options.context,
+    context: this._compiler.options.context,
     content: content
-  });
+  })
 
   // write the file to the output dir
   if (this.emitFile) {
-    this.emitFile(url, content, false);
-    this.addDependency(this.resourcePath);
+    this.emitFile(url, content, false)
+    this.addDependency(this.resourcePath)
   }
 
   // Build the javascript to load the .node at runtime
-  var finalUrl;
+  var finalUrl
   if (config.rewritePath) {
-    finalUrl = JSON.stringify(path.join(config.rewritePath, url));
+    finalUrl = JSON.stringify(path.join(config.rewritePath, url))
   } else if (config.basePath) {
-    var basePath = path.relative(config.basePath, this.options.output.path);
-    finalUrl = JSON.stringify(path.join(basePath, url));
+    var basePath = path.relative(config.basePath, this._compiler.options.output.path)
+    finalUrl = JSON.stringify(path.join(basePath, url))
   } else {
-    finalUrl = "__webpack_public_path__ + " + JSON.stringify(url);
+    finalUrl = '__webpack_public_path__ + ' + JSON.stringify(url)
   }
   if (config.relativePath) {
-    finalUrl = "__dirname + \"/\" + " + finalUrl;
+    finalUrl = '__dirname + "/" + ' + finalUrl
   }
   return (
-    "try { global.process.dlopen(module, " +
+    'try { global.process.dlopen(module, ' +
     finalUrl +
-    "); } catch(e) {" +
+    '); } catch(e) {' +
     "throw new Error('Cannot open ' + " +
     finalUrl +
     " + ': ' + e);}"
-  );
-};
-module.exports.raw = true;
+  )
+}
+module.exports.raw = true
