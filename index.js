5fc042897b68af283db566f2046c736c89f7b9d7
songyihong <songyihong1@163.com>
2020-02-10 17:48:18 +0800

    Update index.js
    support electron windows

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ module.exports = function(content) {
     finalUrl = '__webpack_public_path__ + ' + JSON.stringify(url)
   }
   if (config.relativePath) {
-    finalUrl = '__dirname + "/" + ' + finalUrl
+    finalUrl = process.platform !== 'win32' ? '__dirname + "/" + ' + finalUrl : '__dirname + "\\\\" + ' + finalUrl
   }
   return (
     'try { global.process.dlopen(module, ' +
